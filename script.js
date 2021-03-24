6e889e995c509ee9230618247d1543575eb43480
FoAMDFun <foamdfun@gmail.com>
2021-03-24 17:09:20 +0100

    Objects in Arrays Yee! :)

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 let CurrentPhotoID = 0;
 const MaxPhotoID = 4;
-let Data0 = {
+
+let ImagesData = [{
     Photo: "images/Ferrari.jpg",
     Title: "Ferrari",
     Description: "A high end sportcar from Italy"
-}
-
-let Data1 = {
+},
+{
     Photo: "images/Moon.jpg",
     Title: "The Moon",
     Description: "Our loyal follower"
-}
-
-let Data2 = {
+},
+{
     Photo: "images/cat.png",
     Title: "Cat",
     Description: "She has 9 lives!"
-}
-
-let Data3 = {
+},
+{
     Photo: "images/Eger.jpg",
     Title: "Eger",
     Description: "My Hometown!"
-}
-
-let Data4 = {
+},
+{
     Photo: "images/Palinka.jpg",
     Title: "Pálinka",
     Description: "Covid Vaccine"
-}
-
-let ImagesData = [Data0, Data1, Data2, Data3, Data4];
+}]
 
 $(window).bind('orientationchange', function (event) {
     var SaveID = CurrentPhotoID;
@@ -39,24 +34,24 @@ $(window).bind('orientationchange', function (event) {
     ChangePhoto(CurrentPhotoID);
 });
 
-function ChangePhoto(photoNumber) {
-     $("#Photo").attr("src", ImagesData[photoNumber].Photo);
-     $("#PhotoTitle").text(ImagesData[photoNumber].Title);
-     $("#PhotoDescription").text(ImagesData[photoNumber].Description);
+function ChangePhoto() {
+     $("#Photo").attr("src", ImagesData[CurrentPhotoID].Photo);
+     $("#PhotoTitle").text(ImagesData[CurrentPhotoID].Title);
+     $("#PhotoDescription").text(ImagesData[CurrentPhotoID].Description);
 }
 
 $( "#LeftPanel" ).click(function() {
     if (CurrentPhotoID > 0) {
         CurrentPhotoID--;
     }
-    ChangePhoto(CurrentPhotoID);
+    ChangePhoto();
   });
 
 $( "#RightPanel" ).click(function() {
   if (CurrentPhotoID < MaxPhotoID) {
       CurrentPhotoID++;
   }
-  ChangePhoto(CurrentPhotoID);
+  ChangePhoto();
 });
 
-ChangePhoto(CurrentPhotoID); /* Először az első (Nulladik) elem kiválasztva */
+ChangePhoto(); /* Először az első (Nulladik) elem kiválasztva */

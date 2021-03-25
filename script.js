1d46e23e0ecde9422a1c7113ea4e46fa7ea5f764
FoAMDFun <foamdfun@gmail.com>
2021-03-25 15:05:00 +0100

    Fading images

--- script.js
+++ script.js
@@ -28,6 +28,76 @@ let ImagesData = [{
 }]
 
 const MaxPhotoID = ImagesData.length - 1;
+const PhotoElement = document.querySelector("#Photo");
+
+PhotoElement.addEventListener("animationend", SwapPictures, false);
+
+function ResetAnimations() {
+    $("#Photo").css("animation", "none");
+    $("#Photo").outerHeight();
+    $("#Photo").css("animation", null);
+
+    $("#TransitionPhoto").css("animation", "none");
+    $("#TransitionPhoto").outerHeight();
+    $("#TransitionPhoto").css("animation", null);
+}
+
+function SwapPictures() {
+    // When the picture changing animation is complete we will switch the 2 pictures
+    var ImageName = $("#TransitionPhoto").attr("src");
+
+    $("#Photo").attr("src", ImageName);
+    $("#Photo").css("opacity", "1");
+    $("#Photo").css("z-index", "-1");
+    $("#TransitionPhoto").css("opacity", "0");
+    $("#TransitionPhoto").css("z-index", "-1");
+
+    // We need to reset the animation in order to retrigger it later
+    ResetAnimations();
+}
+
+function InitImage() {
+    $("#Photo").attr("src", ImagesData[CurrentPhotoID].Photo);
+    $("#PhotoTitle").text(ImagesData[CurrentPhotoID].Title);
+    $("#PhotoDescription").text(ImagesData[CurrentPhotoID].Description);
+    $("#Photo").css("opacity", "1");
+    $("#TransitionPhoto").css("opacity", "0");
+    $("#Photo").css("z-index", "-1");
+    $("#TransitionPhoto").css("z-index", "-1");
+}
+
+function ChangePhoto() {
+    // Load picure to Transition first and change the texts down immediately
+
+    $("#TransitionPhoto").attr("src", ImagesData[CurrentPhotoID].Photo);
+    $("#PhotoTitle").text(ImagesData[CurrentPhotoID].Title);
+    $("#PhotoDescription").text(ImagesData[CurrentPhotoID].Description);
+
+    // Animate from #Photo to #TransitionPhoto
+
+    $("#Photo").css("animation-name", "FadeOutPicture");
+    $("#Photo").css("animation-duration", "1s");
+    $("#Photo").css("animation-fill-mode", "forwards");
+    $("#Photo").css("animation-iteration-count", "1");
+    $("#Photo").css("z-index", "-1");
+ 
+    // Animate #TransitionPhoto to show it
+
+    $("#TransitionPhoto").css("animation-name", "FadeInPicture");
+    $("#TransitionPhoto").css("animation-duration", "1s");
+    $("#TransitionPhoto").css("animation-fill-mode", "forwards");
+    $("#TransitionPhoto").css("animation-iteration-count", "1");
+    $("#TransitionPhoto").css("z-index", "-1");
+
+    // After the animation finishes an event handler will swap the pictures
+}
+
+function DisableArrows() {
+    if (CurrentPhotoID = 0) {
+        $("#LeftArrow").css("opacity", "0.1");
+    }
+    
+}
 
 $(window).bind('orientationchange', function (event) {
     var SaveID = CurrentPhotoID;
@@ -37,7 +107,7 @@ $(window).bind('orientationchange', function (event) {
 });
 
 function InitThumbnails() {
-    Dummy = 0;
+    var Dummy = 0;
     ImagesData.forEach(Item => {
         $("#BottomArea").append(`<div class="Thumbnail" data-number=` + Dummy.toString() + `>
         <img class="ThumbnailPhoto" data-number="` + Dummy.toString() + `" src="${Item.Photo}">
@@ -45,32 +115,30 @@ function InitThumbnails() {
         Dummy++;
     })
 }
-function ChangePhoto() {
-     $("#Photo").attr("src", ImagesData[CurrentPhotoID].Photo);
-     $("#PhotoTitle").text(ImagesData[CurrentPhotoID].Title);
-     $("#PhotoDescription").text(ImagesData[CurrentPhotoID].Description);
-}
 
 $( "#LeftPanel" ).click(function() {
     if (CurrentPhotoID > 0) {
         CurrentPhotoID--;
+        ChangePhoto();
     }
-    ChangePhoto();
   });
 
 $("#RightPanel").click(function() {
   if (CurrentPhotoID < MaxPhotoID) {
       CurrentPhotoID++;
+      ChangePhoto();
   }
-  ChangePhoto();
 });
 
 $("body").on("click", ".ThumbnailPhoto", function(Event) {
     let IndexClicked = $(Event.target).attr('data-number');
     let numberIndex = parseInt(IndexClicked);
-    CurrentPhotoID = numberIndex;
-    ChangePhoto();
+    if (CurrentPhotoID != numberIndex) {
+        CurrentPhotoID = numberIndex;
+        ChangePhoto();
+    }
+    
 })
 
 InitThumbnails();
-ChangePhoto(); /* Először az első (Nulladik) elem kiválasztva */
+InitImage();
